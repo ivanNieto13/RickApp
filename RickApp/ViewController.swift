9a3ade4d09640c910311841be5efde2d6c827eb8
Ivan Nieto <ivannieto59@aragon.unam.mx>
2022-10-29 12:38:32 -0500

    Initial Commit

---
+++ RickApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickApp
+//
+//  Created by Ivan Nieto on 29/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
